41cb71f63bd05f5edb14f5875dd83e9934405179
PiotrKruk99-d <biuro@liberezo.pl>
2022-11-09 17:22:19 +0100

    controller test fixed

--- webApi/webApi.Tests/Tests/DndApiControllerTest.cs
+++ webApi/webApi.Tests/Tests/DndApiControllerTest.cs
@@ -1,22 +1,36 @@
 using Xunit;
 using Moq;
 using FluentAssertions;
+using webApi.Api;
 using webApi.Services;
+using webApi.Controllers;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace webApi.Tests;
 
 public class DndApiControllerTests
 {
-    private Mock<IApiService> _service = new Mock<IApiService>();
+    private Mock<IApiService> _service;
+    private Mock<ILogger<DndApiController>> _logger;
+    private DndApiController _controller;
+
+    public DndApiControllerTests()
+    {
+        _service = new Mock<IApiService>();
+        _logger = new Mock<ILogger<DndApiController>>();
+        _controller = new DndApiController(_logger.Object, _service.Object);
+    }
 
     [Fact]
-    public async void GetAllSpellsNamesTest()
+    public async void GetAllSpellsNames_Controller_Test()
     {
         _service.Setup(x => x.GetAllSpells())
-                .ReturnsAsync(new Api.SpellsList() {
+                .ReturnsAsync(new Api.SpellsList()
+                {
                     count = 1,
-                    results = new List<Api.SpellShort>() {
+                    results = new List<SpellShort>() {
                         new Api.SpellShort() {
                             index = "fireball",
                             name = "Fireball",
@@ -24,11 +38,15 @@ public class DndApiControllerTests
                         }
                     }
                 });
-        
-        var result = await _service.Object.GetAllSpells();
+
+        var result = await _controller.GetAllSpellsNames() as ObjectResult;
 
         result.Should().NotBeNull();
-        result!.count.Should().Be(1);
-        result!.results!.ToArray()[0].name.Should().Be("Fireball");
+        result!.Value.Should().NotBeNull();
+        (result!.Value as SpellsList)!.count.Should().Be(1);
+        (result!.Value as SpellsList)!.results.Should().NotBeNull();
+        (result!.Value as SpellsList)!.results!.Count.Should().Be(1);
+        (result!.Value as SpellsList)!.results!.Count.Should().Be(1);
+        (result!.Value as SpellsList)!.results![0].name.Should().Be("Fireball");
     }
 }
